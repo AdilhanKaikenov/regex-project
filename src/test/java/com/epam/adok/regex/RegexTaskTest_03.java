0b794a884d809ea19e6c934e42829ca488fe6827
Adilkhan_Kaikenov <adilhan_kai@mail.ru>
2018-02-09 16:00:45 +0600

    RegexTaskTest_03.java

---
+++ src/test/java/com/epam/adok/regex/RegexTaskTest_03.java
@@ -0,0 +1,51 @@
+package com.epam.adok.regex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(value = Parameterized.class)
+public class RegexTaskTest_03 {
+
+    private String pattern = "\'";
+
+    @Parameter
+    public String input;
+
+    @Parameter(1)
+    public String expected;
+
+    @Parameters(name = "{index}: RegexTaskTest_02({0})")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {"<startElement attribute_01='attribute1'/>", "<startElement attribute_01=\"attribute1\"/>"},
+
+                {"<startElement attribute_01='attribute1'>Some text with quotes : 'Hello Adilhan'</endElement>",
+                        "<startElement attribute_01=\"attribute1\">Some text with quotes : 'Hello Adilhan'</endElement>"},
+
+                {"<startElement attribute_01='attribute1' attribute_02='attribute2'/>",
+                        "<startElement attribute_01=\"attribute1\" attribute_02=\"attribute2\"/>"},
+
+                {"<html:submit styleClass='general-button' onclick='return delete()'><bean:message key='delete.label.button.submit'/>Hello 'Adilkhan'</html:submit>",
+                        "<html:submit styleClass=\"general-button\" onclick=\"return delete()\"><bean:message key=\"delete.label.button.submit\"/>Hello 'Adilkhan'</html:submit>"},
+
+                {"<a attribute_01='attribute1' attribute_02='attribute2'>Русский</a>",
+                        "<a attribute_01=\"attribute1\" attribute_02=\"attribute2\">Русский</a>"}
+
+        });
+    }
+
+    @Test
+    public void testRegex() {
+        assertThat(RegexUtils.replaceAllInHTMLTagElementTo("\"", pattern, input), is(expected));
+    }
+
+}
